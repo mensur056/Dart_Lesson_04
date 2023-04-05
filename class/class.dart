cecd014fb3672c7b955fb9739c50c9d6562534b9
mensur056 <mansur.sarxanov@gmail.com>
2023-04-05 19:31:53 +0400

    test

--- class/class.dart
+++ class/class.dart
@@ -1,9 +1,21 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
+void main() {
+  String item = AppStringConstants.signInText;
+  UserModel userModel = UserModel(surname: "Fikretzade");
+  print(userModel.name);
+}
+
 class UserModel {
-  final String name;
+  final String? name;
   final String surname;
   UserModel({
-    required this.name,
+    this.name = 'No name',
     required this.surname,
   });
+}
+
+class AppStringConstants {
+  static const String signInText = 'Sign in';
+  static const String signUp = 'Sign up';
 }
