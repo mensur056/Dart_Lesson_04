653709024fb53bf466e2b5e4287c8c1ff2bf1c2f
mensur056 <mansur.sarxanov@gmail.com>
2023-04-06 14:30:51 +0400

    test

---
+++ exercise/code.dart
@@ -0,0 +1,24 @@
+// EXERCISE 1  value type
+// Istifadecinin adini ,soyadini ,yasini ve ayliq gelirini saxlayan value type lar yazin amma ad ve soyad hec bir zaman deyise bilmez
+
+
+// EXERCISE 2 if else
+// Bir telebe imtahana daxil olur eger telebe 51 - 61 arasi toplasa E ,61-71 D,71-81 arasi C ,81-91 arasi B, 91-100 arasi A olaraq qiymetlendirilmelidir
+
+
+
+// EXERCISE 3 class and constructor
+// Iki tip class yaradilmalidir ilk class istifadecinin adini soyadini yasini ata adini tutmalidir ve bu class ne zaman istifade olunsa bu parametrleri mutleq yazilmalidir
+// Diger class ise icerisinde static deyerler tutmalidir bu deyerleri istediyiniz bir sey yaza bilersiz bu yazdiginiz datanin ve deyisenlein null ola bilme veziyyetioni de qeyde almalisiz
+
+
+
+
+//EXERCISE 4  Functions
+//  Bir istifadecinin adini yoxlayan bir funksiya yazmalisiz ve bu ad eger "Test" olarsa istifadeciye  adi deyisme icazesi verilmelidir 
+//  ele bir mehtood yazilmalidirki baslangic olan bir ede 5 saniye sonra deyerini 20 vahid artitrmalidir
+
+
+
+//EXERCISE 5 
+//Abstrack class ve impl ve extends istifade ederek bir class dan diger classa mueyyen miras verecek bir class yazin
